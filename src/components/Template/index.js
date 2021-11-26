c7028c29e63b2b5390f4adbd9cc3e08b089dfd99
Ricardo Ireno <postalzrs@gmail.com>
2021-11-26 18:43:21 -0300

    Added tsconfig.json and changed Template component to tsx.

--- src/components/Template/index.js
+++
@@ -1,9 +0,0 @@
-import { NomeDaParada } from './style'
-
-export default function Template( { children } ) {
-	return (
-		<NomeDaParada>
-		  {children}
-		</NomeDaParada>
-		)
-	}
--- src/components/Template/style.js
+++ src/components/Template/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
-export const NomeDaParada = styled.p`
+export const SomeStyled = styled.p`
   font-size: 2rem;
     
-
 `
+
