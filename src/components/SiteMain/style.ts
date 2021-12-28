bf44acbddb1dc38c577242a86dec8c47622d50e7
Ricardo Ireno <ricardoireno@outlook.com>
2021-12-27 22:58:36 -0300

    Removed SiteMain Component

--- src/components/SiteMain/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import theme from '../../styles/theme'
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-`
